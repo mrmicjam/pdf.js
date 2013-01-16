d73c90e83b9d90fa8f08aafb840ff9a4944417a6
Bovard Tiberi <bovard.tiberi+github@webfilings.com>
2013-01-16 13:43:57 -0700

    Added code to replace encoding when using node.

--- src/image.js
+++ src/image.js
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-//TODO: references - window: 1, document: 0
+//TODO: references - window: 0, document: 0
 
 'use strict';
 
@@ -460,7 +460,18 @@ function loadJpegStream(id, imageData, objs) {
   img.onload = (function loadJpegStream_onloadClosure() {
     objs.resolve(id, img);
   });
-  //TODO: bat change the way we do encoding here
-  img.src = 'data:image/jpeg;base64,' + window.btoa(imageData);
+  // http://stackoverflow.com/questions/6182315/how-to-do-base64-encoding-in-node-js
+  var src;
+  if (typeof window !== 'undefined') {
+    // in a window use the window encode
+    src = 'data:image/jpeg;base64,' + window.btoa(imageData);
+  }
+  else if (typeof Buffer !== 'undefined') {
+    // on Node use the node encode
+    src = 'data:image/jpeg;base64,' + new Buffer(imageData).toString('base64');
+  }
+  else
+    console.log("err0r3d!");
+  img.src = src;
 }
 
